46f1495cf90a48dea9a768008de924507291b57d
elro77 <62236356+elro77@users.noreply.github.com>
2020-10-27 15:08:24 +0200

    adding the project

---
+++ MorseCoder/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MorseCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCodedMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 12);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(337, 208);
+            this.tbMessage.TabIndex = 0;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(409, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coded Message";
+            // 
+            // tbCodedMessage
+            // 
+            this.tbCodedMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCodedMessage.Location = new System.Drawing.Point(12, 270);
+            this.tbCodedMessage.Multiline = true;
+            this.tbCodedMessage.Name = "tbCodedMessage";
+            this.tbCodedMessage.Size = new System.Drawing.Size(337, 208);
+            this.tbCodedMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 504);
+            this.Controls.Add(this.tbCodedMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.tbMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCodedMessage;
+    }
+}
+
---
+++ MorseCoder/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace MorseCoder
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer[] soundPlayer= new SoundPlayer[26];
+        private SoundPlayer soundy;
+        public Form1()
+        {
+            InitializeComponent();
+           // soundy = new SoundPlayer("A.wav");
+            soundPlayer[0]= new SoundPlayer("A.wav");
+            soundPlayer[1] = new SoundPlayer("B.wav");
+            soundPlayer[2] = (new SoundPlayer("C.wav"));
+            soundPlayer[3] = (new SoundPlayer("D.wav"));
+            soundPlayer[4] = (new SoundPlayer("E.wav"));
+            soundPlayer[5] = (new SoundPlayer("F.wav"));
+            soundPlayer[6] = (new SoundPlayer("G.wav"));
+            soundPlayer[7] = (new SoundPlayer("H.wav"));
+            soundPlayer[8] = (new SoundPlayer("I.wav"));
+            soundPlayer[9] = (new SoundPlayer("J.wav"));
+            soundPlayer[10] = (new SoundPlayer("K.wav"));
+            soundPlayer[11] = (new SoundPlayer("L.wav"));
+            soundPlayer[12] = (new SoundPlayer("M.wav"));
+            soundPlayer[13] = (new SoundPlayer("N.wav"));
+            soundPlayer[14] = (new SoundPlayer("O.wav"));
+            soundPlayer[15] = (new SoundPlayer("P.wav"));
+            soundPlayer[16] = (new SoundPlayer("Q.wav"));
+            soundPlayer[17] = (new SoundPlayer("R.wav"));
+            soundPlayer[18] = (new SoundPlayer("S.wav"));
+            soundPlayer[19] = (new SoundPlayer("T.wav"));
+            soundPlayer[20] = (new SoundPlayer("U.wav"));
+            soundPlayer[21] = (new SoundPlayer("V.wav"));
+            soundPlayer[22] = (new SoundPlayer("W.wav"));
+            soundPlayer[23] = (new SoundPlayer("X.wav"));
+            soundPlayer[24] = (new SoundPlayer("Y.wav"));
+            soundPlayer[25] = (new SoundPlayer("Z.wav"));
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            String str = "";
+            char[] charArray = tbMessage.Text.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {             
+                if (charArray[i] == ' ')
+                    str+= "\r\n";
+                else
+                {
+                    str +=  addCoded(charArray[i]);
+                }        
+            }
+            tbCodedMessage.Text = str;
+            tbCodedMessage.Update();
+            for (int i=0;i<charArray.Length;i++)
+            {
+                if (charArray[i] == ' ')
+                    Thread.Sleep(500);
+                else
+                {
+                    soundPlayer.ElementAt(charArray[i] - 'a').PlaySync();
+                }
+
+               //soundy.PlaySync();        
+            }
+
+        }
+
+        private String addCoded(char tav)
+        {
+            switch(tav)
+            {
+                case 'a':
+                    return ".- ";
+                case 'b':
+                    return "-... ";
+                case 'c':
+                    return "-.-. ";
+                case 'd':
+                    return "-.. ";
+                case 'e':
+                    return ". ";
+                case 'f':
+                    return "..-. ";
+                case 'g':
+                    return "--. ";
+                case 'h':
+                    return ".... ";
+                case 'i':
+                    return ".. ";
+                case 'j':
+                    return ".--- ";
+                case 'k':
+                    return "-.- ";
+                case 'l':
+                    return ".-.. ";
+                case 'm':
+                    return "-- ";
+                case 'n':
+                    return "-. ";
+                case 'o':
+                    return "--- ";
+                case 'p':
+                    return ".--. ";
+                case 'q':
+                    return "--.- ";
+                case 'r':
+                    return ".-. ";
+                case 's':
+                    return "... ";
+                case 't':
+                    return "- ";
+                case 'u':
+                    return "..- ";
+                case 'v':
+                    return "...- ";
+                case 'w':
+                    return ".-- ";
+                case 'x':
+                    return "-..- ";
+                case 'y':
+                    return "-.-- ";
+                case 'z':
+                    return "--.. ";
+
+            }
+            return " ";
+        }
+    }
+}
